46c8d3a0620f44470f808e98f24d94d7cdf539f3
Brandon S Navone <bn03907@srcs.loc>
2018-03-14 11:22:19 -0700

    new

--- Math Helper/math.js
+++ Math Helper/math.js
@@ -3,24 +3,32 @@ button.addEventListener("click", () => {
     document.getElementById("solutions").textContent = varN_an();
 });
 
+function changeElementClass(id, className) {
+    var el = document.getElementById(id);
+    el.className = className;
+}
+
 function varN_an() {
     var a = document.getElementById('a').value,
         b = document.getElementById('b').value,
-        c = document.getElementById('c').value,
-        a2 = 2 * a,
-        ac = 4 * a * c,
-        bee = Math.pow(b, 2),
-        top = bee - ac;
+        c = document.getElementById('c').value;
 
-    if (bee < 0) {
-        return = 'No real roots exist';
+    var bee = (b * b),
+        ac = (4 * a * c),
+        Sqrt = Math.sqrt(bee - ac),
+        f1 = ((-1 * b) + Sqrt) / (2 * a),
+        f2 = ((-1 * b) - Sqrt) / (2 * a);
+
+    if (isNaN(a) || isNaN(b) || isNaN(c) == true) {
+        changeElementClass("solutions", "invalid");
+        return "You entered an incorrect value for either A, B, or C. Please try again.";
+
+    } else if (bee < ac) {
+        changeElementClass("solutions", "noreal");
+        return "There are no real solutions.";
 
     } else {
-        var bee_sqrt = Math.sqrt(bee);
-        var x1 = -b + bee_sqrt;
-        var x1 = x1 / a2;
-        var x2 = -b - bee_sqrt;
-        var x2 = x2 / a2;
-
+        changeElementClass("solutions", "real");
+        return "The roots to the quadratic are " + f1 + " and " + f2;
     }
 }
